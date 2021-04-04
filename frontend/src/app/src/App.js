a7da7fb98661853cfa1b8f229974f0c802e71f11
Abhinav Sriram <abhinav_sriram@brown.edu>
2021-04-04 13:16:24 -0400

    forgot password fucntionality added

--- frontend/src/app/src/App.js
+++ frontend/src/app/src/App.js
@@ -9,6 +9,7 @@ import HomeScreen from "../../screens/src/HomeScreen";
 import CreateProfileScreen1 from "../../screens/src/CreateProfileScreen1";
 import CreateProfileScreen2 from "../../screens/src/CreateProfileScreen2";
 import ViewProfileScreen from "../../screens/src/ViewProfileScreen";
+import ForgotPasswordScreen from "../../screens/src/ForgotPasswordScreen";
 
 class App extends Component {
 
@@ -20,6 +21,7 @@ class App extends Component {
                         <Route path="/" component={() => <LoadingScreen/>} exact/>
                         <Route path="/landing" component={() => <LandingScreen/>} exact/>
                         <Route path="/signup" component={() => <SignUpScreen/>} exact/>
+                        <Route path="/forgotpassword" component={() => <ForgotPasswordScreen/>} exact/>
                         <Route path="/home" component={() => <HomeScreen/>} exact/>
                         <Route path="/profile1" component={() => <CreateProfileScreen1/>} exact/>
                         <Route path="/profile2" component={() => <CreateProfileScreen2/>} exact/>
---
+++ frontend/src/screens/src/ForgotPasswordScreen.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import '../styles/LandingScreen.css';
+import firebase from '../../firebase.js';
+
+import TextBox from "../../components/src/TextBox";
+import CustomButton from "../../components/src/CustomButton";
+
+class ForgotPasswordScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            errorMessage: "",
+            errorMessageBoolean: false,
+        }
+    }
+
+    changeEmail = (newEmail) => {
+        this.setState({email: newEmail})
+    }
+
+    handleForgotPassword = () => {
+        const email = this.state.email;
+        firebase
+            .auth()
+            .sendPasswordResetEmail(email)
+            .then(() => {
+                this.setState({errorMessageBoolean: false});
+                this.setState({errorMessage: "A password reset link has been sent to your email address, please reset your password and try logging in again. You will be redirected to the login page."});
+                setTimeout(() => window.location.href = "/landing", 5000);
+            })
+            .catch((error) => {
+                this.setState({errorMessageBoolean: true});
+                this.setState({errorMessage: error.message})
+            });
+    };
+
+    render() {
+        return (
+            <div className="main-div">
+                <div className="message-ls">
+                    Forgot Password
+                </div>
+                <TextBox label={"Email"} type={"text"} value={this.state.email} change={this.changeEmail}/>
+                <div style={this.state.errorMessageBoolean ? {color: "red"} : {color: "green"}}
+                     className="error-messages">
+                    {this.state.errorMessage}
+                </div>
+                <br/>
+                <CustomButton value={"Reset Password"} onClick={this.handleForgotPassword}/>
+            </div>
+        );
+    }
+
+}
+
+export default ForgotPasswordScreen;
