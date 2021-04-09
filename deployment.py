2bb2584338acf53870947f28a9e9f5a6ac53efc1
Abhinav Sriram <abhinav_sriram@brown.edu>
2021-04-09 00:04:11 -0400

    wrote master script for heroku deployment

--- deployment.py
+++ deployment.py
@@ -15,6 +15,9 @@ def run_script():
     print(f"{bcolors.OKCYAN}EXECUTING: npm start as bg process{bcolors.ENDC}")
     print(f"{bcolors.WARNING}CHANGE DIRECTORY: os.chdir('./frontend'): in frontend directory{bcolors.ENDC}")
     os.chdir('./frontend')
+    # os.spawnl(os.P_DETACH, 'npm start')
+    # os.spawnl(os.P_NOWAIT, 'npm start')
+    # runs npm start as bg process - may only work on unix based systems
     os.system('npm start &')
     print(f"{bcolors.WARNING}CHANGE DIRECTORY: os.chdir('../'): in project root directory{bcolors.ENDC}")
     os.chdir('../')
@@ -23,5 +26,15 @@ def run_script():
     print(f"{bcolors.OKCYAN}EXECUTING: ./run{bcolors.ENDC}")
     os.system('./run --gui')
     print(f"\n{bcolors.FAIL}TERMINATED: all processes terminated{bcolors.ENDC}")
+
+# def kill_processes():
+#     subprocess = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+#     output, error = subprocess.communicate()
+#     print(output)
+#     target_process = "python"
+#     for line in output.splitlines():
+#         if target_process in str(line):
+#             pid = int(line.split(None, 1)[0])
+#             os.kill(pid, 9)
 
 run_script()
